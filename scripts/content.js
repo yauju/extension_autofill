beaa9c59b8e809bf74f53576989b9c74e9107ff6
yauks <yks@yaukss-MacBook-Air.local>
2026-06-29 15:48:53 +0800

    Initaial commit job-autofill-extension

---
+++ scripts/content.js
@@ -0,0 +1,10 @@
+function autofillfields(input){
+    //check if input field you want to select exists or not 
+    if(!input) {
+        return;
+    }
+
+    const test = "123"
+    const test2 = 123
+
+}
